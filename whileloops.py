b47305c4575c9ba0d144411d178fd048007683ed
unknown <shrikhandare811@gmail.com>
2026-06-29 23:13:35 +0530

    Initial commit: Add Python While loop practice programs

---
+++ whileloops.py
@@ -0,0 +1,75 @@
+#Print all numbers from 1 to 10 using a loop. 
+#i = 1
+#while i<= 10:
+ #   print(i)
+  #  i+= 1
+    
+#Print numbers from 10 down to 1 in reverse order.
+#i=10
+#while i>=1:
+  #  print(i)
+   # i-= 1
+
+#Print all even numbers between 1 and 100.
+#i = 2
+#while i <= 100:
+  #print(i)
+  #i+=2
+   
+#Print all odd numbers between 1 and 100. 
+#i =1
+#while i <=100:
+ #print(i)
+ #i+=2
+
+#Print the multiplication table of a given number from n × 1 to n × 10
+#a = int(input("enter a number:"))
+#i=1
+#while i<=10:
+    #print(a*i)
+    #i+=1
+
+#Calculate and print the sum of the first n natural numbers. 
+#n = int(input("enter a number:"))
+#sum = 0
+#i=1
+#while i<=n:
+ #   sum = sum + i
+  #  i=i+1
+   # print("the sum of first", n, "natural numbers is:", sum)
+
+#Calculate the sum of all even numbers from 1 up to n. 
+#n = int(input("enter a number :"))
+#i=2
+#sum = 0
+#while i<=n:
+ #   sum=sum+i
+  #  i+=2
+#print("the sum of even numbers from 1 to ",n , "is:",sum)
+
+#Calculate the sum of all odd numbers from 1 up to n.
+#n=int(input("enter a number:"))
+#i=1
+#sum=0
+#while i<=n:
+#    sum = sum +i
+ #   i+=2
+#print("the sum of all odd numbers from 1 to", n, "is:", sum)
+
+#Calculate and print the factorial of a given number.
+#n =5
+#fact = 1
+#i =1
+#while i<=n:
+#    fact*=i
+ #   i+=1
+#print("factorial =", fact)
+
+#Find and print the product of all digits of a given number. 
+#n = int(input("enter the number:"))
+#product =1 
+#while n>0:
+   # digit = n%10
+  #  product = product*digit
+ #   n = n//10
+#print("product of all digits is:", product)
